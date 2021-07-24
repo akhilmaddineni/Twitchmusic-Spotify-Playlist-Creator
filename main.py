6909791e5f264c3a727ffefe3e1047e7475cceb4
akhilmaddineni <akhil.maddineni2@gmail.com>
2021-07-24 19:14:43 +0530

    move config to config.txt for release

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import requests
-import json
-from config import *
 from spotify_utils import CreatePlaylist
 import re
 
+#Reading the config.txt file
 
-rest_cmd_url = url+"/"+streamer_id+"/"+rest_query+"?"+response_limit
+import configparser
+
+config = configparser.RawConfigParser()
+config.read('config.txt')
+config_dict = dict(config.items('User_Config'))
+
+print("*******************")
+print(config_dict)
+print("*******************")
+
+url = config_dict['url']
+streamer_id = config_dict['streamer_id'].rstrip()
+rest_query = config_dict['rest_query']
+response_limit = config_dict['response_limit']
+streamer_name_songlist = config_dict['streamer_name_songlist'].rstrip()
+streamer_name_spotify = config_dict['streamer_name_spotify'].rstrip()
+spotify_client_id = config_dict['spotify_client_id'].rstrip()
+spotify_secret = config_dict['spotify_secret'].rstrip()
+playlist_name = config_dict['playlist_name']
+playlist_description = config_dict['playlist_description']
+
+
+
+rest_cmd_url = url + "/" + streamer_id + "/" + rest_query + "?" + response_limit
 rest_api_response = requests.get(rest_cmd_url)
 
-if rest_api_response.status_code != 200 :
-    raise Exception("Communcation with REST API gone bad , please check error code {}".format(rest_api_response.status_code))
-
+if rest_api_response.status_code != 200:
+    raise Exception(
+        "Communcation with REST API gone bad , please check error code {}".format(rest_api_response.status_code))
 
 response_json = rest_api_response.json()
 print(response_json)
@@ -20,9 +42,10 @@ total_num_songs = response_json['total']
 num_pages = total_num_songs // 100
 
 songs_list = []
-for i in range(num_pages+1) :
+for i in range(num_pages + 1):
     rest_cmd_url_temp = rest_cmd_url
-    rest_cmd_url_temp = rest_cmd_url+"&current="+str(i)
+    rest_cmd_url_temp = rest_cmd_url + "&current=" + str(i)
+    print(rest_cmd_url_temp)
     rest_api_response = requests.get(rest_cmd_url_temp)
 
     if rest_api_response.status_code != 200:
@@ -34,34 +57,34 @@ for i in range(num_pages+1) :
 
 print(len(songs_list))
 
-#filtering out the data
+# filtering out the data
 filtered_song_data = []
-def remove_special_chars(text):
-    regrex_pattern = re.compile(pattern = "["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags = re.UNICODE)
-    text = regrex_pattern.sub(r'',text)
 
-    #remove text between two brackets
-    text = re.sub("\(.*?\)","",text)
+
+def remove_special_chars(text):
+    regrex_pattern = re.compile(pattern="["
+                                        u"\U0001F600-\U0001F64F"  # emoticons
+                                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                        "]+", flags=re.UNICODE)
+    text = regrex_pattern.sub(r'', text)
+
+    # remove text between two brackets
+    text = re.sub("\(.*?\)", "", text)
 
     return text
 
-def correct_original_artist_name(text) :
-    return text.replace(streamer_name_songlist,streamer_name_spotify)
 
-for song_info in songs_list :
-    filtered_song_data.append((remove_special_chars(song_info['title'].replace("'", r"\'")),correct_original_artist_name(song_info['artist'].replace("'", r"\'"))))
+def correct_original_artist_name(text):
+    return text.replace(streamer_name_songlist, streamer_name_spotify)
+
+
+for song_info in songs_list:
+    filtered_song_data.append((remove_special_chars(song_info['title'].replace("'", r"\'")),
+                               correct_original_artist_name(song_info['artist'].replace("'", r"\'"))))
 
 print(filtered_song_data)
 
-cp = CreatePlaylist(spotify_client_id,spotify_secret,filtered_song_data,playlist_name,playlist_description)
+cp = CreatePlaylist(spotify_client_id, spotify_secret, filtered_song_data, playlist_name, playlist_description)
 cp.add_to_playlist()
-
-
-
-
-
--- spotify_utils.py
+++ spotify_utils.py
@@ -1,12 +1,10 @@
 import requests
-from config import *
 import json
 
-AUTH_URL = 'https://accounts.spotify.com/api/token'
 
 class CreatePlaylist:
 
-    def __init__(self,user_id,token,song_tuples,playlist_name,playlist_description):
+    def __init__(self, user_id, token, song_tuples, playlist_name, playlist_description):
         self.user_id = user_id
         self.token = token
         self.tuples = song_tuples
@@ -25,8 +23,8 @@ class CreatePlaylist:
             query,
             data=request_body,
             headers={
-                "Content-Type":"application/json",
-                "Authorization":"Bearer {}".format(self.token)
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(self.token)
             }
         )
         response_json = response.json()
@@ -36,8 +34,9 @@ class CreatePlaylist:
 
     # Step 2: Get each song's Spotify uri
     def get_spotify_uri(self, song, artist):
-        try :
-            query = "https://api.spotify.com/v1/search?query=track:%3A{}&type=track&offset=0&limit=1".format(song+" "+artist)
+        try:
+            query = "https://api.spotify.com/v1/search?query=track:%3A{}&type=track&offset=0&limit=1".format(
+                song + " " + artist)
             response = requests.get(
                 query,
                 headers={
@@ -51,28 +50,37 @@ class CreatePlaylist:
 
             # URI
             uri = songs[0]["uri"]
-        except Exception as e :
-            print("Song Info not found in spotify Song %s , Artist %s " % (song,artist))
+        except Exception as e:
+            print("Song Info not found in spotify Song %s , Artist %s " % (song, artist))
             uri = ""
         return uri
+
+    def create_snf_csv_file(self, uri_not_found_list):
+        with open("songs_not_found.csv", 'w') as f:
+            f.write("SongName,ArtistName\n")
+            for song_name, artist_name in uri_not_found_list:
+                f.write(song_name + "," + artist_name + "\n")
 
     # Step 3: Add songs to Spotify Playlist
     def add_to_playlist(self):
         uris = []
+        uri_not_found = []
 
         # Loop through tuples and get URIs
         for i, j in self.tuples:
             uri = self.get_spotify_uri(i, j)
             if uri != "":
                 uris.append(uri)
+            else:
+                uri_not_found.append((i, j))
 
         # Create new playlist
         playlist_id = self.create_playlist()
 
-        uris_100  =[]
-        if len(uris) > 100 :
+        uris_100 = []
+        if len(uris) > 100:
             uris_100 = uris[:100].copy()
-        else :
+        else:
             uris_100 = uris.copy()
 
         # Populate playlist
@@ -89,13 +97,13 @@ class CreatePlaylist:
         )
 
         response_json = response.json()
-        #print(response_json)
+        # print(response_json)
 
-        if len(uris) > 100 :
+        if len(uris) > 100:
             # populate the rest of the songs to the playlist
             divide_uri_list = [uris[i:i + 100] for i in range(0, len(uris), 100)]
             print(len(divide_uri_list))
-            for i in range(1,len(divide_uri_list)) :
+            for i in range(1, len(divide_uri_list)):
                 request_data = json.dumps(divide_uri_list[i])
                 query = "https://api.spotify.com/v1/playlists/{}/tracks".format(playlist_id)
                 response = requests.post(
@@ -107,6 +115,9 @@ class CreatePlaylist:
                     }
                 )
                 response_json = response.json()
-                #print(response_json)
+                # print(response_json)
+
+        # create a csv file for songs not found
+        self.create_snf_csv_file(uri_not_found)
 
         return response_json
